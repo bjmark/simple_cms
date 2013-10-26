2db10c259409b18c14198d2f0aee818ac12e4d89
pengweimin <bjmark@sina.com>
2013-10-26 11:55:41 +0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCms::Application.config.secret_token = '2bd8eaadf71c249e57bdcb07c8bd2205dc98134b6de3465a91d7e698601ea3946ad39cb20fe644a5d899f5154fa29cefdfbffc2e74477af30a6cba563099a62e'
---
+++ g.sh
@@ -0,0 +1,17 @@
+#rails generate rspec:install
+#rails g model note
+#rails g model tag, it is a mistake
+#rails g model category
+#rails g model categories_2_notes
+#rake db:schema:dump
+#RAILS_ENV=test rake db:schema:load
+#rails g integration_test search
+#rails g controller notes 
+
+#rails g model search
+#rails g model operation
+#rails g controller operations 
+
+#rails g migration add_path_to_search 
+#rails g model content_index
+#rails g model background_job
